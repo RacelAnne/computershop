be2943d1696af0889ff21a9d8265fd2335759626
Racel Anne Pitogo <racelanne19gmail.com>
2022-10-06 13:21:35 +0800

    Initial

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import streamlit as st
+
+st.set_page_config(page_title="Our Computer Shop", page_icon=":wave:", layout="wide")
+
+st.subheader("Hi, I am RACEL :wave:")
+st.title("Computer Parts")
+st.write("Testing")
